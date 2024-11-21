ab334b24962fbc3568278b2f066baabdec897acf
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2024-11-21 11:05:03 +0100

    Add columns loop

--- 2/chessboard.js
+++ 2/chessboard.js
@@ -3,5 +3,11 @@ let size = 8;
 let board = "";
 
 for(let y = 0; y < size; y++) {
-    console.log(y);
+    for (let x = 0; x < size; x++) {
+        if ((y + x) % 2 == 0) {
+            console.log("#");
+        } else {
+            console.log(" ");
+        }
+    }
 }
