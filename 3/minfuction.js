e622bb6ef98506f4eb4a0ab63de6e34f81385ebd
airstrike-mike <139953019+airstrike-mike@users.noreply.github.com>
2024-11-27 15:25:51 +0100

    Solve exercise

---
+++ 3/minfuction.js
@@ -0,0 +1,11 @@
+function min (a, b) {
+    if (a < b) {
+        console.log(a);
+    } else if (a > b) {
+        console.log(b);
+    } else {
+        console.log("Numbers are equal");
+    }
+}
+
+min(100, -10);
